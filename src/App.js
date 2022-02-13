d4bc492c7bee23afa7c68447fc48843695f78344
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-13 15:45:51 +0800

    Add functionality to search bar

--- src/App.js
+++ src/App.js
@@ -11,17 +11,22 @@ const App = () => {
   return (
     <div>
       <Search value={search} setValue={setSearch} />
-      {data.map((project) => (
-        <Project
-          key={project.id}
-          banner={project.banner}
-          name={project.name}
-          description={project.description}
-          tags={project.tags}
-          source={project.source}
-          demo={project.demo}
-        />
-      ))}
+      {data
+        .filter((project) => {
+          const filter = `${project.tags.toString()},${project.name}`;
+          return filter.toLowerCase().includes(search.toLowerCase());
+        })
+        .map((project) => (
+          <Project
+            key={project.id}
+            banner={project.banner}
+            name={project.name}
+            description={project.description}
+            tags={project.tags}
+            source={project.source}
+            demo={project.demo}
+          />
+        ))}
     </div>
   );
 };
