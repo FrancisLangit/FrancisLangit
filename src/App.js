0176ee231f096f9679974d5c98015636e9a7366e
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-13 14:01:54 +0800

    Create module to hold data on projects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { data } from './data.js';
 import { Project } from './components/Project/Project';
 
 const App = () => {
   return (
     <div>
-      <Project
-        banner="https://repository-images.githubusercontent.com/390632472/e593c066-4ae7-42ce-b38f-d7720885a215"
-        name="Mug Wumps"
-        description="A bastardized version of battleship with inspirations taken from the 1970's video game, Mugwump. Playable as a web-application."
-        technologies={['HTML', 'CSS', 'SASS', 'JavaScript']}
-        repository="https://github.com/FrancisLangit/mug-wumps"
-        demo="https://francislangit.github.io/mug-wumps/"
-      />
+      {data.map((project) => (
+        <Project
+          banner={project.banner}
+          name={project.name}
+          description={project.description}
+          technologies={project.technologies}
+          repository={project.repository}
+          demo={project.demo}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    banner:
+      'https://repository-images.githubusercontent.com/390632472/e593c066-4ae7-42ce-b38f-d7720885a215',
+    name: 'Mug Wumps',
+    description:
+      "A bastardized version of battleship with inspirations taken from the 1970's video game, Mugwump. Playable as a web-application.",
+    technologies: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+    repository: 'https://github.com/FrancisLangit/mug-wumps',
+    demo: 'https://francislangit.github.io/mug-wumps/',
+  },
+  {
+    banner:
+      'https://repository-images.githubusercontent.com/390632472/e593c066-4ae7-42ce-b38f-d7720885a215',
+    name: 'Mug Wumps',
+    description:
+      "A bastardized version of battleship with inspirations taken from the 1970's video game, Mugwump. Playable as a web-application.",
+    technologies: ['HTML', 'CSS', 'SASS', 'JavaScript'],
+    repository: 'https://github.com/FrancisLangit/mug-wumps',
+    demo: 'https://francislangit.github.io/mug-wumps/',
+  },
+];
+
+export { data };
