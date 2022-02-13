d57c0f08f14ea3a76677968607abfae85c00d2c4
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-13 13:39:36 +0800

    Add props to Project component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { Project } from './components/Project/Project';
 const App = () => {
   return (
     <div>
-      <Project />
+      <Project
+        banner="https://repository-images.githubusercontent.com/390632472/e593c066-4ae7-42ce-b38f-d7720885a215"
+        name="Mug Wumps"
+        description="A bastardized version of battleship with inspirations taken from the 1970's video game, Mugwump. Playable as a web-application."
+        technologies={['HTML', 'CSS', 'SASS', 'JavaScript']}
+        repository="https://github.com/FrancisLangit/mug-wumps"
+        demo="https://francislangit.github.io/mug-wumps/"
+      />
     </div>
   );
 };
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,27 @@
-const Project = () => {
-  return <div>I'm a project!</div>;
+const Project = ({
+  banner,
+  name,
+  description,
+  technologies,
+  repository,
+  demo,
+}) => {
+  return (
+    <div>
+      <img src={banner} alt={name + 'banner'} />
+      <div>{name}</div>
+      <div>{description}</div>
+      {technologies.map((technology) => (
+        <div>{technology}</div>
+      ))}
+      <a href={repository}>
+        <div>Source Code</div>
+      </a>
+      <a href={demo}>
+        <div>Demo</div>
+      </a>
+    </div>
+  );
 };
 
 export { Project };
