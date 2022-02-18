e7be681195c5666e10e0b7f4c581644133807c96
Francis Langit <francis.villanueva.langit@gmail.com>
2022-02-18 09:45:53 +0800

    Delete Search component and dependencies

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import { useState } from 'react';
-
 import { data } from './data.js';
 import style from './App.module.css';
 
 import { Header } from './components/Header/Header.js';
 import { Project } from './components/Project/Project';
-import { Search } from './components/Search/Search.js';
 
 const App = () => {
-  const [search, setSearch] = useState('');
-
   return (
     <div>
       <Header />
-      <Search value={search} setValue={setSearch} />
       <div className={style.projects}>
-        {data
-          .filter((project) => {
-            const filter = `${project.tags.toString()},${project.name}`;
-            return filter.toLowerCase().includes(search.toLowerCase());
-          })
-          .map((project) => (
-            <Project
-              key={project.id}
-              name={project.name}
-              banner={project.banner}
-              link={project.link}
-            />
-          ))}
+        {data.map((project) => (
+          <Project
+            key={project.id}
+            banner={project.banner}
+            link={project.link}
+          />
+        ))}
       </div>
     </div>
   );
